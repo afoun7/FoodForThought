40015cb88ee6f93040e78dc5f1f7a3adfffa91f6
Amanda Foun <afoun@wellesley.edu>
2016-03-08 00:05:04 -0500

    registration form complete

--- food.py
+++ food.py
@@ -1,12 +1,15 @@
 # Amanda Foun
 
-from flask import Flask, render_template, request, jsonify, redirect, flash
+from flask import Flask, render_template, request, jsonify, redirect, flash, session
 from flask.ext.login import LoginManager, UserMixin, login_required, login_user, logout_user, url_for
 from forms import LoginForm  
 from user import User
 from flask.ext.wtf import Form  
-from wtforms import StringField, PasswordField  
+from wtforms import StringField, PasswordField, Form, BooleanField, TextField, validators
 from wtforms.validators import DataRequired
+from werkzeug.security import generate_password_hash
+from pymongo.errors import DuplicateKeyError
+from pymongo import MongoClient
 import recipeQueries
 import sys
 import operator
@@ -18,7 +21,8 @@ app.config.from_object('config')
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-# LOGIN INFORMATION
+
+# LOGIN 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -30,15 +34,15 @@ def login():
         	login_user(user_obj)
         	flash("Logged in successfully!", category='success')
         	return redirect(request.args.get("next") or url_for("search")) # checks if user has permission to access next url
-        print "yes"
         flash("Wrong username or password!", category='error') # could not find in database
-        print "tes2"
     return render_template('login.html', title='login', form=form)
+
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('login')) # go back to login page
+
 
 @login_manager.user_loader # user_loader callback is used to reload the user object from the user ID stored in the session
 def load_user(username): 
@@ -48,6 +52,51 @@ def load_user(username):
 		return None
 	return User(u['_id'])
 
+# SIGN UP/REGISTRATION
+
+# This is the actual form that we want to connect to our webpage
+class RegistrationForm(Form):
+    username = TextField('Username', [validators.Length(min=4, max=20)])
+    email = TextField('Email Address', [validators.Length(min=6, max=50)])
+    password = PasswordField('New Password', [
+        validators.Required(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])  # must be equal to 'confirm', else error message is Passwords must match
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the Terms of Service and Privacy Notice', [validators.Required()]) # checkbox
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_page():
+    try:
+        form = RegistrationForm(request.form) # allows us to render the form
+
+        if request.method == "POST" and form.validate(): # if user hit register button and form is complete
+            username = form.username.data # refers to username from RegistrationForm class
+            email = form.email.data
+            pass_hash = generate_password_hash(form.password.data, method='pbkdf2:sha256') # hash the user's pw
+
+            try:
+                collection = MongoClient()["test"]["users"] # Connect to the DB
+                collection.insert({"_id": username, "password": pass_hash})
+                flash("Thank you for registering!")
+                session['logged_in'] = True
+                session['username'] = username
+                print "in here!"
+                return redirect(url_for("search"))
+
+            except DuplicateKeyError: 
+                print "sucks"
+                flash("Username already taken. Please choose another.")
+                return render_template('register.html', form=form)
+        
+        return render_template('register.html', form=form)
+    
+    except Exception as e:
+        return(str(e))
+
+
+# OTHER
 @app.route('/')
 @app.route('/search' , methods=['POST', 'GET'])
 def search():
@@ -57,12 +106,16 @@ def search():
  		results = recipeQueries.get_matches(query)
 	return render_template('search.html' ,results=results)
 
+
 @app.route('/newsfeed' , methods=['POST', 'GET'])
 @login_required # need to login to see newsfeed
 def newsfeed():
 	return render_template('newsfeed.html')
 
-
+@app.route('/test')
+def test():
+    flash("IT IS WORKING YAY")
+    return
 
 if __name__ == '__main__':
     app.run(debug=True)
