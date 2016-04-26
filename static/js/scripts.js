1de985dbfead52505cabd68793a61b346bdab884
Amanda Foun <afoun@wellesley.edu>
2016-04-26 10:10:39 -0400

    calendar renderEvent not working

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,28 +1,62 @@
-  $(document).on('pageshow','#index',function(e,data){    
-        var date = new Date();
-        var d = date.getDate();
-        var m = date.getMonth();
-        var y = date.getFullYear();
-    
-        $('#calendar').fullCalendar({
-            editable: true,
-            events: [
-                {
-                    title: 'Meal',
-                    start: new Date(y, m, d, 10, 30),
-                    allDay: false
-                },
-                {
-                    title: 'Meal',
-                    start: new Date(y, m, d, 12, 0),
-                    end: new Date(y, m, d, 14, 0),
-                    allDay: false
-                }
-            ]
-        });
+$(document).ready(function() {
+    var date = new Date();
+    var d = date.getDate();
+    var m = date.getMonth();
+    var y = date.getFullYear();
+    var calendar = $('#calendar').fullCalendar({
+      defaultView: 'basicWeek',
+      editable: true,
+      selectable: true,
     });
 
+    $('#submitButton').on('click', function(e){
+    // We don't want this to act as a link so cancel the link action
+    e.preventDefault();
 
+    doSubmit();
+  });
+
+    window.doSubmit = function() {
+         $("#add").popup('close');
+        console.log($('#meal').val());
+        console.log($('#date').val());
+  
+        var d = new Date($('#date').val());
+        if (($('#meal').val()) == "breakfast") {
+            d.setHours(7);
+        };
+    
+        $("#calendar").fullCalendar('addEventSource',
+            {
+                title: $('#recipeName').val(),
+                start: d,
+                end: d.setMinutes(d.getMinutes() + 60)
+            },
+            true);
+        $("#calendar").fullCalendar('rerenderEvents');
+
+       };
+}); 
+
+
+// function doSubmit(){
+//         $("#add").popup('close');
+//         console.log($('#meal').val());
+//         console.log($('#date').val());
+//         alert("form submitted");
+
+//         $("#calendar").fullCalendar('renderEvent',
+//             {
+//                 title: $('#recipeName').val(),
+//                 start: new Date($('#date').val()),
+//             },
+//             true);
+//         $("#calendar").fullCalendar('rerenderEvents');
+//         alert("added to calendar");
+//        };
+
+
+// allergy form
 $(document).ready(function() {
      $('#has_allergies').change(function() {
         if(this.value=="has_allergies") {
@@ -31,6 +65,7 @@ $(document).ready(function() {
     })
  });
 
+// indicate other allergies
 $(document).ready(function() {
      $("#other").change(function () {
         //check if it's checked. If checked move inside and check for others value
