3de03953a10167738dd6404d04fbdf54cf901e8b
Jasmin Rubinovitz <jasrub@media.mit.edu>
2016-07-08 13:26:01 -0400

    remove extra files debugging

--- food.py
+++ food.py
@@ -249,16 +249,16 @@ def list(recipe_id):
     return render_template('list.html',recipe=recipe)
 
 # not for production. for debugging
-extra_dirs = ['templates','static']
-extra_files = extra_dirs[:]
-for extra_dir in extra_dirs:
-    for dirname, dirs, files in os.walk(extra_dir):
-        for filename in files:
-            filename = os.path.join(dirname, filename)
-            if os.path.isfile(filename):
-                extra_files.append(filename)
+# extra_dirs = ['templates','static']
+# extra_files = extra_dirs[:]
+# for extra_dir in extra_dirs:
+#     for dirname, dirs, files in os.walk(extra_dir):
+#         for filename in files:
+#             filename = os.path.join(dirname, filename)
+#             if os.path.isfile(filename):
+#                 extra_files.append(filename)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True, extra_files=extra_files)
+    app.run(debug=True, use_reloader=True)
     
